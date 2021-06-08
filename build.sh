25ed1ed1cd8ff2be7cae474fe5ea3a051ed55791
Markus Reiter <me@reitermark.us>
2021-06-08 11:16:49 +0200

    Refactor build script.

--- build.sh
+++ build.sh
@@ -5,10 +5,17 @@ set -euo pipefail
 : "${image:=reitermarkus/zero-scale-proxy}"
 : "${cache_image:=${image}:cache}"
 
+if [[ "${1-}" = '--push' ]]; then
+  cache_to=( --cache-to "type=registry,ref=${cache_image},mode=max" )
+else
+  cache_to=()
+fi
+
 docker buildx create --name mybuilder || true
 docker buildx use mybuilder
 docker buildx build . \
   --platform linux/amd64,linux/arm64 \
   --cache-from "type=registry,ref=${cache_image}" \
+  "${cache_to[@]}" \
   --tag "${image}" \
   "${@}"
